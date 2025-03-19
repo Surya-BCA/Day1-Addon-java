02b08490bba5009741d24e172eb66199e9fe4dfc
Surya-BCA <2322ja56@kgcas.com>
2025-03-19 14:02:56 +0530

    arrays

---
+++ ja56/Day1.java topics/Arraysinjava/src/core/ArrayMainClass.java
@@ -0,0 +1,13 @@
+package core;
+
+public class ArrayMainClass {
+
+	public static void main(String[] args) {
+		
+		onedimensional  oneD = new onedimensional();
+		oneD.display();
+		oneD.Displayusingforloop();
+		
+	}
+
+}
